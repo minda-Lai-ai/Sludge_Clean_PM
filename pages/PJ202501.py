1071f82d2e2d81f15a8cab50851850948e5fd7f7
minda-Lai-ai <laimindar@gmail.com>
2025-11-17 15:22:16 +0800

    Update PJ202501.py

--- pages/PJ202501.py
+++ pages/PJ202501.py
@@ -1,12 +1,16 @@
 import streamlit as st
 from datetime import date
 
-st.set_page_config(page_title="XX專案1 - PJ202501 主頁", layout="wide")
+from modules.insurance import render_insurance
+from modules.meeting import render_meeting
+from modules.sludge import render_sludge
+from modules.work import render_work
+from modules.member import render_member
 
+st.set_page_config(page_title="XX專案1 - PJ202501 主頁", layout="wide")
 st.title("XX專案1 - PJ202501 主頁")
 st.link_button("回到主選單", "/")
 
-# 專案基本資訊
 basic_info = {
     "工作案號": "PJ202501",
     "預算編號": "B12345",
@@ -24,35 +28,23 @@ basic_info = {
 for k, v in basic_info.items():
     st.write(f"{k}: {v}")
 
-# 專案狀況判斷
-project_status = ""
 today = date.today()
 start = date(2025,1,1)
 end = date(2025,1,31)
-if start <= today <= end:
-    project_status = "進行中"
-elif today < start:
-    project_status = "準備中"
-elif today > end:
-    project_status = "延遲"
+if start <= today <= end: project_status = "進行中"
+elif today < start: project_status = "準備中"
+elif today > end: project_status = "延遲"
 st.write(f"專案狀況: {project_status}")
 
 st.divider()
 subpage = st.radio("次頁面", ["保險", "會議", "公證量油", "施工紀要", "成員矩陣"])
-
-# 分頁資料互動區
 if subpage == "保險":
-    from insurance import render_insurance
-    render_insurance(project_no=basic_info["工作案號"])
+    render_insurance(basic_info["工作案號"])
 elif subpage == "會議":
-    from meeting import render_meeting
-    render_meeting(project_no=basic_info["工作案號"])
+    render_meeting(basic_info["工作案號"])
 elif subpage == "公證量油":
-    from sludge import render_sludge
-    render_sludge(project_no=basic_info["工作案號"])
+    render_sludge(basic_info["工作案號"])
 elif subpage == "施工紀要":
-    from work import render_work
-    render_work(project_no=basic_info["工作案號"])
+    render_work(basic_info["工作案號"])
 elif subpage == "成員矩陣":
-    from member import render_member
-    render_member(project_no=basic_info["工作案號"])
+    render_member(basic_info["工作案號"])
